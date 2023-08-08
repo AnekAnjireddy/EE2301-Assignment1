10a405e26a35f43568338269ebb9262311360102
Anek A <aneka@Sakalas-MacBook-Air.local>
2023-08-08 21:13:56 +0530

    1/2/2

--- codes/q1.py
+++ codes/q1.py
@@ -3,6 +3,10 @@ A=np.array([1,-1])
 B=np.array([-4,6])
 C=np.array([-3,-5])
 
-print("The parametric of AB form is x:",A,"+ k",B-A)
-print("The parametric of BC form is x:",B,"+ k",C-B)
-print("The parametric of CA form is x:",C,"+ k",A-C)
+D=np.array([(B[0]+C[0])/2, (B[1]+C[1])/2])
+E=np.array([(C[0]+A[0])/2, (C[1]+A[1])/2])
+F=np.array([(A[0]+B[0])/2, (A[1]+B[1])/2])
+
+print("The parametric of AB form is x:",A,"+ k",D-A)
+print("The parametric of BC form is x:",B,"+ k",E-B)
+print("The parametric of CA form is x:",C,"+ k",F-C)
