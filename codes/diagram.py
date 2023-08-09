557bb326652c7b8037d9a58b747e65652c98dfb2
Anek A <aneka@Sakalas-MacBook-Air.local>
2023-08-09 18:17:13 +0530

    1/2/2

---
+++ codes/diagram.py
@@ -0,0 +1,64 @@
+import sys
+sys.path.insert(0, "./codes/CoordGeo")
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from line.funcs import *
+
+#vertices of triangle
+
+A=np.array([1, -1])
+B=np.array([-4, 6])
+C=np.array([-3, -5])
+
+#generating lines
+
+x_AB = line_gen(A, B)
+x_BC = line_gen(B, C)
+x_CA = line_gen(C, A)
+
+#generating midpoints
+
+D = (B+C)/2
+E = (A+C)/2
+F = (A+B)/2
+
+#generating medians
+
+x_AD = line_gen(A, D)
+x_BE = line_gen(B, E)
+x_CF = line_gen(C, F)
+
+#plotting all lines
+
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.plot(x_BC[0,:],x_BC[1,:],label='$BC$')
+plt.plot(x_CA[0,:],x_CA[1,:],label='$CA$')
+plt.plot(x_AD[0,:],x_AD[1,:],label='$AD$')
+plt.plot(x_BE[0,:],x_BE[1,:],label='$BE$')
+plt.plot(x_CF[0,:],x_CF[1,:],label='$CF$')
+
+
+
+
+#labeling points
+
+tri_coords = np.block([[A],[B],[C],[D],[E],[F]]).T
+plt.scatter(tri_coords[0,:], tri_coords[1,:])
+vert_labels = ['A','B','C','D', 'E','F']
+for i, txt in enumerate(vert_labels):
+    plt.annotate(txt,
+                 (tri_coords[0,i], tri_coords[1,i]),
+                 textcoords="offset points", 
+                 xytext=(0,10), 
+                 ha='center') 
+
+
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid() 
+plt.axis('equal')
+
+plt.savefig('./figs/Diagram.png')
